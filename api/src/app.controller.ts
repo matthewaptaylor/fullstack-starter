2d0cedd7cdcdad7c7784e48eafc4bc8766f6e91b
Matthew Taylor <matthewaptaylor@gmail.com>
2023-09-30 12:46:55 +1300

    chore: remove unused endpoints

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,23 +1,7 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from '@/app.service';
-import { CreateUserDto } from '@/CreateUser.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
-  /**
-   * Returns a stringified version of the CreateUserDto object.
-   * @param createUserDto The CreateUserDto object.
-   * @returns A stringified version of the CreateUserDto object.
-   */
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return JSON.stringify(createUserDto);
-  }
 }
